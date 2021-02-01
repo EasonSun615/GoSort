10b8080683137140a34595d0e414eceb4209d741
sunyirong <yirongsun@tencent.com>
2021-02-01 11:42:57 +0800

    feat:增加merge节点

--- cmd/pipelinedemo/main.go
+++ cmd/pipelinedemo/main.go
@@ -5,26 +5,37 @@ import (
 	"goProject/pipeline"
 )
 
-
 func main1() {
-	src := pipeline.ArraySource(3,45,1,4,5,5,31,5)
+	src := pipeline.ArraySource(3, 45, 1, 4, 5, 5, 31, 5)
 	// for v := range src {
 	// 	fmt.Println(v)
 	// }
 	for {
 		if c, ok := <-src; ok {
 			fmt.Println(c)
-		}else{
+		} else {
 			break
 		}
 	}
 }
 
-func main(){
+func main2() {
 	p := pipeline.InMemSort(
-		pipeline.ArraySource(4,1,6,12,7,2,9,41,6))
+		pipeline.ArraySource(4, 1, 6, 12, 7, 2, 9, 41, 6))
 	// GoLang不需要锁，如果out里的数据没有准备好，会自动阻塞
-	for v := range p{
+	for v := range p {
+		fmt.Println(v)
+	}
+}
+
+func main() {
+	p := pipeline.Merge(
+		pipeline.InMemSort(
+			pipeline.ArraySource(4, 1, 6, 12, 7, 2, 9, 41, 6)),
+		pipeline.InMemSort(
+			pipeline.ArraySource(324,45,1,3,4,5123,7,5)))
+	// GoLang不需要锁，如果out里的数据没有准备好，会自动阻塞
+	for v := range p {
 		fmt.Println(v)
 	}
 }
--- pipeline/node.go
+++ pipeline/node.go
@@ -2,7 +2,7 @@ package pipeline
 
 import "sort"
 
-func ArraySource(a ...int) <- chan int {
+func ArraySource(a ...int) <-chan int {
 	out := make(chan int)
 	go func() {
 		for _, v := range a {
@@ -13,10 +13,10 @@ func ArraySource(a ...int) <- chan int {
 	return out
 }
 
-func InMemSort(in <- chan int) <- chan int {
+func InMemSort(in <-chan int) <-chan int {
 	out := make(chan int)
 	go func() {
-		a := []int {}
+		a := []int{}
 		// Read into memory
 		for v := range in {
 			a = append(a, v)
@@ -30,5 +30,25 @@ func InMemSort(in <- chan int) <- chan int {
 		}
 		close(out)
 	}()
+	return out
+}
+
+func Merge(in1, in2 <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		num1, ok1 := <-in1
+		num2, ok2 := <-in2
+		for ok1 || ok2 {
+			if !ok2 || (ok1 && num1 < num2) {
+				out <- num1
+				num1, ok1 = <-in1
+			} else {
+				out <- num2
+				num2, ok2 = <-in2
+			}
+		}
+		close(out)
+	}()
+
 	return out
 }
